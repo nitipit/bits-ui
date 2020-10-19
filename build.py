1650b3b18483cfa6552d6e20724883de80a71deb
Nitipit Nontasuwan <nitipit@gmail.com>
2020-10-19 18:00:26 +0700

    update

--- build.py
+++ build.py
@@ -8,11 +8,11 @@ base_dir = Path(__file__).parent
 
 async def packet_ui():
     proc = await asyncio.create_subprocess_shell(
-        'yarn run parcel build src/packet-ui.js')
+        'npx parcel build src/packet-ui.js')
     await proc.communicate()
 
     proc = await asyncio.create_subprocess_shell(
-        'yarn run sass src/:dist/')
+        'npx sass src/:dist/')
     await proc.communicate()
 
     src_dir = base_dir.joinpath('src/style')
@@ -26,7 +26,7 @@ async def packet_ui():
 
 async def lib():
     proc = await asyncio.create_subprocess_shell(
-        'yarn run parcel build node_modules/normalize.css/normalize.css')
+        'npx parcel build node_modules/normalize.css/normalize.css')
     await proc.communicate()
 
 
