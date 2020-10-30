50962bb14dfe7c87b7988cc8e915ecca2458701a
Nitipit Nontasuwan <nitipit@gmail.com>
2020-10-30 16:06:02 +0700

    update

--- build.py
+++ build.py
@@ -3,44 +3,60 @@ from pathlib import Path
 import shutil
 
 
-base_dir = Path(__file__).parent
+class PacketUI:
+    def __init__(
+            self, src_dir: Path,
+            dist_dir: Path,
+            node_modules_dir: Path):
+        self.src_dir = src_dir.resolve()
+        self.dist_dir = dist_dir.resolve()
+        self.node_modules_dir = node_modules_dir
 
+    async def build(self):
+        # Build packet-ui.js
+        proc = await asyncio.create_subprocess_shell(
+            f"npx parcel build --dist-dir {self.dist_dir} "
+            f"{self.src_dir.joinpath('packet-ui.js')}"
+        )
+        await proc.communicate()
 
-async def packet_ui():
-    proc = await asyncio.create_subprocess_shell(
-        'npx parcel build src/packet-ui.js')
-    await proc.communicate()
+        # Build SCSS
+        proc = await asyncio.create_subprocess_shell(
+            f"npx sass {self.src_dir}:{self.dist_dir}"
+        )
+        await proc.communicate()
 
-    proc = await asyncio.create_subprocess_shell(
-        'npx sass src/:dist/')
-    await proc.communicate()
+        # Copy SCSS
+        src_dir = self.src_dir.joinpath('style')
+        dest_dir = self.dist_dir.joinpath('style')
+        shutil.copytree(src_dir, dest_dir, dirs_exist_ok=True)
 
-    src_dir = base_dir.joinpath('src/style')
-    dest_dir = base_dir.joinpath('dist/style')
-    shutil.copytree(src_dir, dest_dir, dirs_exist_ok=True)
+        # Copy UI JS & SCSS
+        src_dir = self.src_dir.joinpath('ui')
+        dest_dir = self.dist_dir.joinpath('ui')
+        shutil.copytree(src_dir, dest_dir, dirs_exist_ok=True)
+        proc = await asyncio.create_subprocess_shell(
+            f"npx parcel build --dist-dir {self.dist_dir} "
+            f"{self.node_modules_dir.joinpath('normalize.css/normalize.css')}"
+        )
+        await proc.communicate()
 
-    src_dir = base_dir.joinpath('src/ui')
-    dest_dir = base_dir.joinpath('dist/ui')
-    shutil.copytree(src_dir, dest_dir, dirs_exist_ok=True)
-
-
-async def lib():
-    proc = await asyncio.create_subprocess_shell(
-        'npx parcel build node_modules/normalize.css/normalize.css')
-    await proc.communicate()
-
-
-async def font():
-    shutil.copytree(
-        base_dir.joinpath('src/font'),
-        base_dir.joinpath('dist/font'),
-        dirs_exist_ok=True,
-    )
+        # Copy Fonts
+        shutil.copytree(
+            self.src_dir.joinpath('font'),
+            self.dist_dir.joinpath('font'),
+            dirs_exist_ok=True,
+        )
 
 
 async def main():
-    await packet_ui()
-    await lib()
-    await font()
+    base_dir = Path(__file__).parent
+    packet_ui = PacketUI(
+        src_dir=base_dir.joinpath('src'),
+        dist_dir=base_dir.joinpath('dist'),
+        node_modules_dir=base_dir.joinpath('node_modules')
+    )
+    await packet_ui.build()
+
 
 asyncio.run(main())
